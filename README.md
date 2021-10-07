43c38b568f59777cc00a9fde97712bbbd8d9428e
BlackStorry <franco@groegl.de>
2021-10-07 08:57:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zjq6bd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zjq6bd)
---
+++ src/app/components/show-list/show-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Show } from '../../model/show';
+@Component({
+  selector: 'app-show-list',
+  templateUrl: './show-list.component.html',
+  styleUrls: ['./show-list.component.css']
+})
+export class ShowListComponent implements OnInit {
+  shows: Show[] = [];
+  constructor() { 
+    this.shows.push(new Show(1, 'Paw Patrol'));
+  }
+
+  ngOnInit() {
+  }
+
+}
